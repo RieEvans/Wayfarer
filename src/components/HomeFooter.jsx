42477d1117067223265a85795961b98b17ab1cea
Riejan Evangelista <riejanevangelista1620@gmail.com>
2023-07-09 13:15:23 +0800

    Updated Wayfarer Final

--- src/components/HomeFooter.jsx
+++ src/components/HomeFooter.jsx
@@ -6,9 +6,7 @@ const Footer = () => {
     <footer className= {`bg-slate-900 w-full mt-5`}>
       <div className={` bg-no-repeat bg-top bg-cover m-auto w-[70%] md:h-[9rem] sm:leading-8 py-10`}>
         <p className="text-emerald-400 text-center ">
-          All material herein © 2023 RJC Company Pte. Ltd. All Rights
-          Reserved.<br/> <br/>  RJC is part of Booking Holdings Inc. ,the world leader in
-          online travel & related services.
+          © Riejan Evangelista CpE | July 8, 2023
         </p>
       </div>
     </footer>
